d9c6ff7ddbf7e3bb73e51d2f631598d7bc3be68d
markmybytes <markmybytes@gmail.com>
2025-02-13 17:15:39 +0800

    Fix dataset fetching task

--- app/src/main.py
+++ app/src/main.py
@@ -1,17 +1,97 @@
 import os
+from multiprocessing.pool import Pool
+from pathlib import Path
 
-from fastapi import FastAPI
+from apscheduler.executors.pool import ProcessPoolExecutor, ThreadPoolExecutor
+from apscheduler.jobstores.memory import MemoryJobStore
+from apscheduler.schedulers.background import BackgroundScheduler
+from fastapi import APIRouter, FastAPI
 from fastapi.staticfiles import StaticFiles
+from pytz import UTC
+from app.src import definition
 
-from app.src import schedules
-from app.src.routers import eta, route, icon
+from app.src.modules import hketa
+from app.src.routers import eta, icon, route
 
 app = FastAPI(title="HKETA-API-Server", debug=True)
 
 app.mount("/static", StaticFiles(directory=os.path.join(
     os.path.dirname(os.path.dirname(__file__)), "static")), name="static")
 
-app.include_router(schedules.router)
+
+# initialisation
+scheduler = None
+
+
+@app.on_event("startup")
+async def init_scheduler():
+    global scheduler
+    jobstores = {
+        "default": MemoryJobStore(),
+    }
+    executors = {
+        "default": ThreadPoolExecutor(15),
+        "processpool": ProcessPoolExecutor(5),
+    }
+    job_defaults = {"coalesce": False, "max_instances": 3}
+
+    scheduler = BackgroundScheduler(
+        jobstores=jobstores,
+        executors=executors,
+        job_defaults=job_defaults,
+        timezone=UTC,
+    )
+    scheduler.start()
+
+    def fetch_dataset_job():
+        with Pool(os.cpu_count() or 4) as p:
+            p.map([
+                hketa.predictor.KmbPredictor(definition.DATASET_PATH, definition.ETA_FACTORY.create_transport(
+                    hketa.enums.Company.KMB)).fetch_dataset(),
+                hketa.predictor.MtrBusPredictor(definition.DATASET_PATH, definition.ETA_FACTORY.create_transport(
+                    hketa.enums.Company.MTRBUS)).fetch_dataset()
+            ])
+
+    # import threading
+    # threading.Thread(target=fe, args=[scheduler]).start()
+
+    # ---------- init tasks ----------
+    scheduler.add_job(fetch_dataset_job,
+                      trigger='cron',
+                      minute='*/1',
+                      id='kmb_ml_fetch_raw')
+    # scheduler.add_job(hketa.predictor.KmbPredictor(definition.DATASET_PATH,
+    #                                                hketa_factory.create_transport(
+    #                                                    hketa.enums.Company.KMB)
+    #                                                ).raws_to_ml_dataset,
+    #                   args=['day'],
+    #                   trigger='cron',
+    #                   hour='3',
+    #                   minute='0',
+    #                   id='kmb_ml_raw2dataset_day')
+    # scheduler.add_job(hketa.predictor.KmbPredictor(definition.DATASET_PATH,
+    #                                                hketa_factory.create_transport(
+    #                                                    hketa.enums.Company.KMB)
+    #                                                ).raws_to_ml_dataset,
+    #                   args=['night'],
+    #                   trigger='cron',
+    #                   hour='15',
+    #                   minute='0',
+    #                   id='kmb_ml_raw2dataset_night')
+    # scheduler.add_job(await hketa.predictor.MtrBusPredictor(definition.DATASET_PATH,
+    #                                                         hketa_factory.create_transport(
+    #                                                             hketa.enums.Company.MTRBUS)
+    #                                                         ).fetch_dataset,
+    #                   trigger='cron',
+    #                   minute='*/1',
+    #                   id='kmb_ml_fetch_raw')
+
+
+@app.on_event("shutdown")
+def shutdown_scheduler():
+    scheduler.shutdown(wait=False)
+
+
 app.include_router(eta.router)
 app.include_router(route.router)
 app.include_router(icon.router)
--- app/src/modules/hketa/predictor.py
+++ app/src/modules/hketa/predictor.py
@@ -1,8 +1,9 @@
+from abc import ABC, abstractmethod
 import asyncio
 import json
 import math
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from multiprocessing.pool import Pool, ThreadPool
 from pathlib import Path
 from typing import Literal, Optional
@@ -13,21 +14,54 @@ import pytz
 import sklearn.tree
 
 try:
-    from app.src.modules.hketa import api, api_async
+    from app.src.modules.hketa import api, api_async, transport
 except (ImportError, ModuleNotFoundError):
     import api
     import api_async
+    import transport
 
 
-class KmbPredictor:
+class Predictor(ABC):
 
+    __path_prefix__: str
+
+    def __init__(self, directory: os.PathLike[str], transport_: transport.Transport) -> None:
+        self.transport_ = transport_
+        self.root_dir = Path(str(directory)) \
+            .joinpath(self.__path_prefix__ or self.__class__.__name__.lower())
+        self.raws_dir = self.root_dir.joinpath('raws')
+
+        if not self.root_dir.exists():
+            os.makedirs(self.root_dir)
+
+    def _calculate_etas_error(df: pd.DataFrame) -> pd.DataFrame:
+        for _, group in df.groupby('stop'):
+            schedules = []
+            last_tta, last_timestamp = float('inf'), None
+
+            for row in group.itertuples():
+                if row.tta > last_tta:
+                    for index, eta in schedules:
+                        error = (eta - last_timestamp).total_seconds()
+                        if math.isnan(error) or abs(error) > 7200:
+                            # 1. malformated timestamp will result int float('nan')
+                            # 2. ignore unusual TTA
+                            continue
+                        df.loc[index, ['accuracy']] = round(error / 60)
+                    schedules = []
+                    last_tta, last_timestamp = float('inf'), None
+
+                schedules.append((row.Index, row.eta))
+                last_tta = row.tta
+                last_timestamp = row.data_timestamp
+        return df
+
+
+class KmbPredictor(Predictor):
+
+    __path_prefix__ = 'kmb'
     _HEADS = ['co', 'route', 'dir', 'service_type', 'seq', 'dest_tc', 'dest_sc',
               'dest_en', 'eta_seq', 'eta', 'rmk_tc', 'rmk_sc', 'rmk_en', 'data_timestamp']
-
-    def __init__(self, directory: os.PathLike[str],) -> None:
-        self.root_dir = Path(str(directory))
-        if not self.root_dir.exists():
-            self.root_dir.mkdir()
 
     def predict(self,
                 route: str,
@@ -69,48 +103,20 @@ class KmbPredictor:
 
             return eta
 
-        with open(Path(__file__).parent.joinpath('kmb.json'), 'r', encoding='utf-8') as f:
-            routes = json.load(f)
-
-        with ThreadPool(15) as pool:
-            results = pool.map(
-                lambda p: api.kmb_eta(*p)['data'], [(r, 1) for r in routes])
-
-        filename = self.root_dir.joinpath('raws.csv')
-        if not os.path.exists(filename):
-            old_df = pd.DataFrame(columns=self._HEADS)
-        else:
-            old_df = pd.read_csv(filename, index_col=0)
-
-        df = pd.DataFrame(
-            [entry_cleaning(eta)
-                for eta in (eta for route in results for eta in route)
-                if eta['eta'] is not None],
-            columns=self._HEADS)
-
-        pd.concat([old_df, df[df['eta_seq'] == 1]]) \
-            .reset_index(drop=True) \
-            .to_csv(filename, mode='w', index=True)
-
-    async def fetch_dataset_async(self) -> None:
-        def entry_cleaning(eta: dict) -> dict:
-            try:
-                # malformed data_timestamp is save to be replaced by the current time
-                # while eta not, which will be ignore later at `process_raw_dataset`
-                datetime.fromisoformat(eta['data_timestamp'])
-            except (ValueError, TypeError):
-                eta['data_timestamp'] = datetime.now(
-                    pytz.timezone('Asia/Hong_kong')).isoformat(timespec='seconds')
-
-            return eta
+        async def fetch():
+            async with aiohttp.ClientSession() as s:
+                return await asyncio.gather(*[
+                    api_async.kmb_eta(r, 1, s) for r in routes
+                ])
 
         with open(Path(__file__).parent.joinpath('kmb.json'), 'r', encoding='utf-8') as f:
             routes = json.load(f)
 
-        async with aiohttp.ClientSession() as s:
-            results = await asyncio.gather(*[
-                api_async.kmb_eta(r, 1, s) for r in routes
-            ])
+        # async with aiohttp.ClientSession() as s:
+        #     results = await asyncio.gather(*[
+        #         api_async.kmb_eta(r, 1, s) for r in routes
+        #     ])
+        results = asyncio.run(fetch())
 
         filename = self.root_dir.joinpath('raws.csv')
         if not os.path.exists(filename):
@@ -188,37 +194,96 @@ class KmbPredictor:
             .drop(df[df['accuracy'] == ''].index, errors='ignore') \
             .to_csv(filename, mode='a', index=False)
 
-    def _calculate_etas_error(self, df: pd.DataFrame) -> pd.DataFrame:
-        for _, group in df.groupby('seq'):
-            schedules = []
-            last_tta, last_timestamp = float('inf'), None
 
-            for row in group.itertuples():
-                if row.tta > last_tta:
-                    for index, eta in schedules:
-                        error = (eta - last_timestamp).total_seconds()
-                        if math.isnan(error) or abs(error) > 7200:
-                            # 1. malformated timestamp will result int float('nan')
-                            # 2. ignore unusual TTA
-                            continue
-                        df.loc[index, ['accuracy']] = round(error / 60)
-                    schedules = []
-                    last_tta, last_timestamp = float('inf'), None
+class MtrBusPredictor(Predictor):
+    _HEADS = ['route', 'dir', 'eta_seq', 'stop', 'eta', 'data_timestamp']
 
-                schedules.append((row.Index, row.eta))
-                last_tta = row.tta
-                last_timestamp = row.data_timestamp
-        return df
+    async def fetch_dataset(self) -> None:
+        def entry_cleaning(eta: dict) -> dict:
+            try:
+                # malformed data_timestamp is save to be replaced by the current time
+                # while eta not, which will be ignore later at `process_raw_dataset`
+                datetime.fromisoformat(eta['data_timestamp'])
+            except (ValueError, TypeError):
+                eta['data_timestamp'] = datetime.now(
+                    pytz.timezone('Asia/Hong_kong')).isoformat(timespec='seconds')
+            return eta
 
-    def _append_dataset(self, new_df: pd.DataFrame, filename: os.PathLike) -> pd.DataFrame:
-        df = pd.read_csv(self.root_dir.joinpath(str(filename)))
+        filename = self.raws_dir.joinpath('raws.csv')
+        if not os.path.exists(filename):
+            old_df = pd.DataFrame(columns=self._HEADS)
+        else:
+            old_df = pd.read_csv(filename, index_col=0)
 
-        if len(df['month'].unique()) > 3:
-            del_y = sorted(df['year'].unique())[0]
-            del_m = sorted(df[df['year'].eq(del_y)]['month'].unique())[0]
-            del_d = sorted(
-                df.loc[df['month'].eq(del_m) & df['year'].eq(del_y)]['day'].unique())[0]
-            df = df.drop(
-                df[df['year'].eq(del_y) & df['month'].eq(del_m) & df['day'].eq(del_d)].index)
+        async with aiohttp.ClientSession() as e:
+            all_eta = await asyncio.gather(
+                *[api_async.mtr_bus_eta(r, 'en', e) for r in self.transport_.route_list().keys()])
 
-        return pd.concat([df, new_df])
+        etas = []
+        data_timestamp = datetime.now()
+        for route in all_eta:
+            for stop in route['busStop']:
+                for idx, eta in enumerate(stop['bus']):
+                    eta_second = int(eta['departureTimeInSecond']) \
+                        if any(s in stop['busStopId'] for s in ('U010', 'D010')) \
+                        else int(eta['arrivalTimeInSecond'])
+                    dir = 'O' \
+                        if stop['busStopId'].split('-')[1].startswith('U') \
+                        else 'I'
+
+                    etas.append({
+                        'route': route['routeName'],
+                        'stop': stop['busStopId'],
+                        'dir': dir,
+                        'eta_seq': idx + 1,
+                        'data_timestamp': data_timestamp.isoformat(timespec='seconds'),
+                        'eta': (data_timestamp + timedelta(seconds=eta_second)).isoformat(timespec='seconds')
+                    })
+
+        df = pd.DataFrame(etas, columns=self._HEADS)
+        pd.concat([old_df, df[df['eta_seq'] == 1]]) \
+            .reset_index(drop=True) \
+            .to_csv(filename, mode='w', index=True)
+
+    def raws_to_ml_dataset(self, type_: Literal['day', 'night']) -> None:
+        if type_ == 'night':
+            return
+        if type_ != 'day':
+            raise ValueError(f'Incorrect type: {type_}.')
+
+        df = pd.read_csv(Path(__file__).parent.joinpath('raws.csv'),
+                         on_bad_lines='warn',
+                         low_memory=False,
+                         index_col=[0])
+
+        with Pool(os.cpu_count() or 4) as pool:
+            pool.starmap(self._process_raw_dataset,
+                         [g for g in df.groupby('route')])
+
+    def _process_raw_dataset(self, route: str, df: pd.DataFrame) -> None:
+        df[['eta', 'data_timestamp']] = df[['eta', 'data_timestamp']] \
+            .apply(pd.to_datetime, format='ISO8601', cache=True, errors='coerce')
+
+        df = df.assign(year=df['data_timestamp'].dt.year,
+                       month=df['data_timestamp'].dt.month,
+                       day=df['data_timestamp'].dt.day,
+                       hour=df['data_timestamp'].dt.hour,
+                       minute=df['data_timestamp'].dt.minute,
+                       eta_hour=df['eta'].dt.hour,
+                       eta_minute=df['eta'].dt.minute,
+                       is_weekend=(
+            df['data_timestamp'].dt.weekday >= 5).astype(int),
+            tta=(df['eta'] - df['data_timestamp']
+                 ).dt.total_seconds(),
+            accuracy='')
+
+        self._generate_ml_dataset(
+            df[df['dir'] == 'O'], Path(__file__).parent.joinpath('mtr_bus', f'{route}_outbound.csv'))
+        self._generate_ml_dataset(
+            df[df['dir'] == 'I'], Path(__file__).parent.joinpath('mtr_bus', f'{route}_inbound.csv'))
+
+    def _generate_ml_dataset(self, df: pd.DataFrame, filename: Path) -> None:
+        self._calculate_etas_error(df) \
+            .drop(columns=['dir', 'eta', 'data_timestamp'], errors='ignore') \
+            .drop(df[df['accuracy'] == ''].index, errors='ignore') \
+            .to_csv(filename, mode='a', index=False, header=not filename.exists())
