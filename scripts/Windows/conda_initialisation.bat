72dfb50d0825e68ebb1b423f8d7a0badeed145ae
markmybytes <markmybytes@gmail.com>
2025-02-13 17:15:19 +0800

    add command check to script

--- scripts/Windows/conda_initialisation.bat
+++ scripts/Windows/conda_initialisation.bat
@@ -1,6 +1,13 @@
 @echo off
 set /p ENVNAME=<"%~dp0..\ENVNAME"
 
+REM Check wheather anaconda is installed.
+WHERE /q conda
+IF ERRORLEVEL 1 (
+    echo "Error: anaconda is not installed, quitting..."
+    exit 1
+)
+
 echo "Creating conda enviroment %ENVNAME% with Python version 3.11"
 echo:
 call conda create --name %ENVNAME% python=3.11 -y
