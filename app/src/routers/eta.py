1198ed38425c5d7b7ebadc04bb7807b283529d14
markmybytes <markmybytes@gmail.com>
2025-02-13 17:15:23 +0800

    incl route info in eta response

--- app/src/routers/eta.py
+++ app/src/routers/eta.py
@@ -31,6 +31,12 @@ def get_eta(company: hketa.enums.Company,
 
         return std_response.StdResponse.success(
             data={
+                'route': route_name,
+                'orig': provider.details.origin(),
+                'dest': provider.details.destination(),
+                'direction': direction.value,
+                'stop': provider.details.stop_name(),
+                'logo': "",
                 'etas': provider.etas()
             }
         )
