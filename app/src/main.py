88483b6f03fd160669e4081d72ba4e17fa68d7a5
markmybytes <markmybytes@gmail.com>
2025-02-13 17:15:59 +0800

    Increase time between `perpare_ml_dataset`

--- app/src/main.py
+++ app/src/main.py
@@ -60,8 +60,8 @@ async def init_scheduler():
             definition.DATASET_PATH, definition.ETA_FACTORY.create_transport(hketa.enums.Company.MTRBUS)) \
             .raws_to_ml_dataset(type_)
 
-    @scheduler.scheduled_job(trigger='cron', args=['day'], hour='3', minute='5', max_instances=1)
-    @scheduler.scheduled_job(trigger='cron', args=['night'], hour='15', minute='5', max_instances=1)
+    @scheduler.scheduled_job(trigger='cron', args=['day'], hour='3', minute='15', max_instances=1)
+    @scheduler.scheduled_job(trigger='cron', args=['night'], hour='15', minute='15', max_instances=1)
     def perpare_ml_dataset_kmb(type_: Literal['day', 'night']):
         hketa.predictor.KmbPredictor(
             definition.DATASET_PATH, definition.ETA_FACTORY.create_transport(hketa.enums.Company.KMB)) \
